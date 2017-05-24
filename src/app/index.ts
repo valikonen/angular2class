6b8806c820c4c32499dfd84c438d363b990193c6
Dragos Apostol <dragos.apostol1@ro.ibm.com>
2017-05-24 10:48:24 +0300

    Add config and gitignore

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,7 @@
 import * as services from './services';
 export { AppComponent } from './app.component';
 
-const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
+export const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
 
 export const providers = [
     ...mapValuesToArray(services)
